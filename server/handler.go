7798b9ffa1f35a6c48364527a32ebad42a3ae136
Adrian Shum <adrian@cshum.com>
2021-12-02 15:06:16 +0800

    refactor server handlers

--- server/handler.go
+++ server/handler.go
@@ -1,13 +1,69 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"go.uber.org/zap"
+	"net/http"
+	"strconv"
+)
 
-var handleFavicon = get("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+func handleFavicon(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	return
-})
+}
 
-var handleHealth = get("/health", func(w http.ResponseWriter, r *http.Request) {
+func handleHealth(w http.ResponseWriter, r *http.Request) {
 	resJSON(w, GetHealthStats())
 	return
-})
+}
+
+type Error struct {
+	Message string `json:"message,omitempty"`
+	Code    int    `json:"status,omitempty"`
+}
+
+func (s *Server) panicHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if rvr := recover(); rvr != nil {
+				err, ok := rvr.(error)
+				if !ok {
+					err = fmt.Errorf("%v", rvr)
+				}
+				s.Logger.Error("panic", zap.Error(err))
+				w.WriteHeader(http.StatusInternalServerError)
+				resJSON(w, Error{
+					Message: err.Error(),
+					Code:    http.StatusInternalServerError,
+				})
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func resJSON(w http.ResponseWriter, v interface{}) {
+	buf, _ := json.Marshal(v)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Length", strconv.Itoa(len(buf)))
+	w.Write(buf)
+	return
+}
+
+func pathHandler(method string, handleFuncs map[string]http.HandlerFunc) Middleware {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != method {
+				next.ServeHTTP(w, r)
+				return
+			}
+			if handle, ok := handleFuncs[r.URL.Path]; ok {
+				handle(w, r)
+				return
+			}
+			next.ServeHTTP(w, r)
+			return
+		})
+	}
+}
--- server/server.go
+++ server/server.go
@@ -1,10 +1,8 @@
 package server
 
 import (
-	"encoding/json"
 	"go.uber.org/zap"
 	"net/http"
-	"strconv"
 	"time"
 )
 
@@ -22,47 +20,14 @@ func New(handler http.Handler, options ...Option) *Server {
 	s.MaxHeaderBytes = 1 << 20
 	s.Logger = zap.NewNop()
 
-	s.Handler = route(
-		handleFavicon,
-		handleHealth,
-	)(handler)
-
+	s.Handler = pathHandler(http.MethodGet, map[string]http.HandlerFunc{
+		"/favicon.ico": handleFavicon,
+		"/health":      handleHealth,
+	})(handler)
 	for _, option := range options {
 		option(s)
 	}
+	s.Handler = s.panicHandler(s.Handler)
+
 	return s
-}
-
-func resJSON(w http.ResponseWriter, v interface{}) {
-	buf, _ := json.Marshal(v)
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Content-Length", strconv.Itoa(len(buf)))
-	w.Write(buf)
-	return
-}
-
-func route(middlewares ...Middleware) Middleware {
-	return func(handler http.Handler) http.Handler {
-		ln := len(middlewares)
-		for i := ln - 1; i >= 0; i-- {
-			handler = middlewares[i](handler)
-		}
-		return handler
-	}
-}
-
-func handleMethod(method, path string, handler http.HandlerFunc) Middleware {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.Method != method || path != r.URL.Path {
-				next.ServeHTTP(w, r)
-				return
-			}
-			handler(w, r)
-		})
-	}
-}
-
-func get(path string, handler http.HandlerFunc) Middleware {
-	return handleMethod(http.MethodGet, path, handler)
 }
