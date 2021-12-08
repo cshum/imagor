bdf9c3ef5689b33cce4f2910f4c72c0d4fc6c89c
Adrian Shum <adrian@cshum.com>
2021-12-08 16:14:05 +0800

    prepare imagoruri.Generate

---
+++ imagoruri/generate.go
@@ -0,0 +1,97 @@
+package imagoruri
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+const (
+	HAlignLeft   = "left"
+	HAlignRight  = "right"
+	VAlignTop    = "top"
+	VAlignBottom = "bottom"
+)
+
+func generate(p Params) string {
+	var parts []string
+	if p.Meta {
+		parts = append(parts, "meta")
+	}
+	if p.Trim != "" {
+		trims := []string{"trim"}
+		if br := "bottom-right"; p.Trim == br {
+			trims = append(trims, br)
+		}
+		if p.TrimTolerance > 0 {
+			trims = append(trims, strconv.Itoa(p.TrimTolerance))
+		}
+		parts = append(parts, strings.Join(trims, ":"))
+	}
+	if p.CropTop > 0 || p.CropRight > 0 || p.CropLeft > 0 || p.CropBottom > 0 {
+		parts = append(parts, fmt.Sprintf(
+			"%dx%d:%dx%d", p.CropLeft, p.CropTop, p.CropRight, p.CropBottom))
+	}
+	if p.FitIn {
+		parts = append(parts, "fit-in")
+	}
+	if p.Stretch {
+		parts = append(parts, "stretch")
+	}
+	if p.Upscale {
+		parts = append(parts, "upscale")
+	}
+	if p.HorizontalFlip || p.Width != 0 || p.VerticalFlip || p.Height != 0 {
+		if p.Width < 0 {
+			p.HorizontalFlip = !p.HorizontalFlip
+			p.Width = -p.Width
+		}
+		if p.Height < 0 {
+			p.VerticalFlip = !p.VerticalFlip
+			p.Height = -p.Height
+		}
+		var hFlipStr, vFlipStr string
+		if p.HorizontalFlip {
+			hFlipStr = "-"
+		}
+		if p.VerticalFlip {
+			vFlipStr = "-"
+		}
+		parts = append(parts, fmt.Sprintf(
+			"%s%dx%s%d", hFlipStr, p.Width, vFlipStr, p.Height))
+	}
+	if p.HPadding > 0 || p.VPadding > 0 {
+		parts = append(parts, fmt.Sprintf("%dx%d", p.HPadding, p.VPadding))
+	}
+	if p.HAlign == HAlignLeft || p.HAlign == HAlignRight {
+		parts = append(parts, p.HAlign)
+	}
+	if p.VAlign == VAlignTop || p.VAlign == VAlignBottom {
+		parts = append(parts, p.VAlign)
+	}
+	if p.Smart {
+		parts = append(parts, "smart")
+	}
+	if len(p.Filters) > 0 {
+		var filters []string
+		for _, f := range p.Filters {
+			filters = append(filters, fmt.Sprintf("%s(%s)", f.Name, f.Args))
+		}
+		parts = append(parts, "filters:"+strings.Join(filters, ":"))
+	}
+	if strings.Contains(p.Image, "?") {
+		p.Image = url.QueryEscape(p.Image)
+	}
+	parts = append(parts, p.Image)
+	return strings.Join(parts, "/")
+}
+
+func GenerateUnsafe(p Params) string {
+	return "unsafe/" + generate(p)
+}
+
+func Generate(p Params, secret string) string {
+	path := generate(p)
+	return Sign(path, secret) + "/" + path
+}
--- imagoruri/params_test.go
+++ imagoruri/params_test.go
@@ -7,17 +7,17 @@ import (
 	"testing"
 )
 
-func TestParse(t *testing.T) {
+func TestParseGenerate(t *testing.T) {
 	tests := []struct {
-		name     string
-		uri      string
-		expected Params
-		secret   string
+		name   string
+		uri    string
+		params Params
+		secret string
 	}{
 		{
 			name: "non url image",
-			uri:  "/meta/10x11:12x13/fit-in/-300x-200/left/top/smart/filters:some_filter()/img",
-			expected: Params{
+			uri:  "meta/10x11:12x13/fit-in/-300x-200/left/top/smart/filters:some_filter()/img",
+			params: Params{
 				Path:           "meta/10x11:12x13/fit-in/-300x-200/left/top/smart/filters:some_filter()/img",
 				Image:          "img",
 				CropLeft:       10,
@@ -38,8 +38,8 @@ func TestParse(t *testing.T) {
 		},
 		{
 			name: "url image",
-			uri:  "/meta/10x11:12x13/fit-in/upscale/-300x-200/left/top/smart/filters:some_filter()/s.glbimg.com/es/ge/f/original/2011/03/29/orlandosilva_60.jpg",
-			expected: Params{
+			uri:  "meta/10x11:12x13/fit-in/upscale/-300x-200/left/top/smart/filters:some_filter()/s.glbimg.com/es/ge/f/original/2011/03/29/orlandosilva_60.jpg",
+			params: Params{
 				Path:           "meta/10x11:12x13/fit-in/upscale/-300x-200/left/top/smart/filters:some_filter()/s.glbimg.com/es/ge/f/original/2011/03/29/orlandosilva_60.jpg",
 				Image:          "s.glbimg.com/es/ge/f/original/2011/03/29/orlandosilva_60.jpg",
 				CropLeft:       10,
@@ -61,8 +61,8 @@ func TestParse(t *testing.T) {
 		},
 		{
 			name: "url in filter",
-			uri:  "/filters:watermark(s.glbimg.com/es/ge/f/original/2011/03/29/orlandosilva_60.jpg,0,0,0)/img",
-			expected: Params{
+			uri:  "filters:watermark(s.glbimg.com/es/ge/f/original/2011/03/29/orlandosilva_60.jpg,0,0,0)/img",
+			params: Params{
 				Path:    "filters:watermark(s.glbimg.com/es/ge/f/original/2011/03/29/orlandosilva_60.jpg,0,0,0)/img",
 				Image:   "img",
 				Filters: []Filter{{Name: "watermark", Args: "s.glbimg.com/es/ge/f/original/2011/03/29/orlandosilva_60.jpg,0,0,0"}},
@@ -70,8 +70,8 @@ func TestParse(t *testing.T) {
 		},
 		{
 			name: "multiple filters",
-			uri:  "/filters:watermark(s.glbimg.com/es/ge/f/original/2011/03/29/orlandosilva_60.jpg,0,0,0):brightness(-50):grayscale()/img",
-			expected: Params{
+			uri:  "filters:watermark(s.glbimg.com/es/ge/f/original/2011/03/29/orlandosilva_60.jpg,0,0,0):brightness(-50):grayscale()/img",
+			params: Params{
 				Path:  "filters:watermark(s.glbimg.com/es/ge/f/original/2011/03/29/orlandosilva_60.jpg,0,0,0):brightness(-50):grayscale()/img",
 				Image: "img",
 				Filters: []Filter{
@@ -91,8 +91,8 @@ func TestParse(t *testing.T) {
 		},
 		{
 			name: "no params",
-			uri:  "/unsafe/https://thumbor.readthedocs.io/en/latest/_images/man_before_sharpen.png",
-			expected: Params{
+			uri:  "unsafe/https://thumbor.readthedocs.io/en/latest/_images/man_before_sharpen.png",
+			params: Params{
 				Path:   "https://thumbor.readthedocs.io/en/latest/_images/man_before_sharpen.png",
 				Image:  "https://thumbor.readthedocs.io/en/latest/_images/man_before_sharpen.png",
 				Unsafe: true,
@@ -100,10 +100,10 @@ func TestParse(t *testing.T) {
 		},
 		{
 			name: "url in filters",
-			uri:  "/unsafe/stretch/500x350/filters:watermark(http://thumborize.me/static/img/beach.jpg,100,100,50)/http://thumborize.me/static/img/beach.jpg?v=93ce8775572809c2fa498f3ba53c9ef6",
-			expected: Params{
-				Path:    "stretch/500x350/filters:watermark(http://thumborize.me/static/img/beach.jpg,100,100,50)/http://thumborize.me/static/img/beach.jpg?v=93ce8775572809c2fa498f3ba53c9ef6",
-				Image:   "http://thumborize.me/static/img/beach.jpg?v=93ce8775572809c2fa498f3ba53c9ef6",
+			uri:  "unsafe/stretch/500x350/filters:watermark(http://thumborize.me/static/img/beach.jpg,100,100,50)/http://thumborize.me/static/img/beach.jpg",
+			params: Params{
+				Path:    "stretch/500x350/filters:watermark(http://thumborize.me/static/img/beach.jpg,100,100,50)/http://thumborize.me/static/img/beach.jpg",
+				Image:   "http://thumborize.me/static/img/beach.jpg",
 				Width:   500,
 				Height:  350,
 				Unsafe:  true,
@@ -118,9 +118,9 @@ func TestParse(t *testing.T) {
 		},
 		{
 			name:   "non url image with hash",
-			uri:    "/VTAq7YIRbEXgtwAcsTMhAjvBuT8=/meta/10x11:12x13/fit-in/-300x-200/5x6/left/top/smart/filters:some_filter()/img",
+			uri:    "VTAq7YIRbEXgtwAcsTMhAjvBuT8=/meta/10x11:12x13/fit-in/-300x-200/5x6/left/top/smart/filters:some_filter()/img",
 			secret: "1234",
-			expected: Params{
+			params: Params{
 				Path:           "meta/10x11:12x13/fit-in/-300x-200/5x6/left/top/smart/filters:some_filter()/img",
 				Hash:           "VTAq7YIRbEXgtwAcsTMhAjvBuT8=",
 				Image:          "img",
@@ -150,12 +150,25 @@ func TestParse(t *testing.T) {
 		t.Run(strings.TrimPrefix(test.name, "/"), func(t *testing.T) {
 			resp := Parse(test.uri)
 			respJSON, _ := json.MarshalIndent(resp, "", "  ")
-			expectedJSON, _ := json.MarshalIndent(test.expected, "", "  ")
-			if !reflect.DeepEqual(resp, test.expected) {
+			expectedJSON, _ := json.MarshalIndent(test.params, "", "  ")
+			if !reflect.DeepEqual(resp, test.params) {
 				t.Errorf(" = %s, want %s", string(respJSON), string(expectedJSON))
 			}
 			if test.secret != "" && Sign(resp.Path, test.secret) != resp.Hash {
 				t.Errorf("signature mismatch = %s, want %s", resp.Hash, Sign(resp.Path, test.secret))
+			}
+			if test.params.Hash != "" {
+				if uri := Generate(test.params, test.secret); uri != test.uri {
+					t.Errorf(" = %s, want = %s", uri, test.uri)
+				}
+			} else if test.params.Unsafe {
+				if uri := GenerateUnsafe(test.params); uri != test.uri {
+					t.Errorf(" = %s, want = %s", uri, test.uri)
+				}
+			} else {
+				if uri := generate(test.params); uri != test.uri {
+					t.Errorf(" = %s, want = %s", uri, test.uri)
+				}
 			}
 		})
 	}
