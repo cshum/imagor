2cc20e7a629bd93c6fdb636ee6ee58a4e1ec8303
Adrian Shum <adrian@cshum.com>
2021-12-17 18:22:40 +0800

    test: imagor TestWithUnsafe

---
+++ imagor_test.go
@@ -0,0 +1,28 @@
+package imagor
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func jsonStr(v interface{}) string {
+	buf, _ := json.Marshal(v)
+	return string(buf)
+}
+
+func TestWithUnsafe(t *testing.T) {
+	app := New(WithUnsafe(true))
+	w := httptest.NewRecorder()
+	app.ServeHTTP(w, httptest.NewRequest(
+		http.MethodGet, "https://example.com/unsafe/foo.jpg", nil))
+	assert.Equal(t, 200, w.Code)
+
+	w = httptest.NewRecorder()
+	app.ServeHTTP(w, httptest.NewRequest(
+		http.MethodGet, "https://example.com/foo.jpg", nil))
+	assert.Equal(t, 403, w.Code)
+	assert.Equal(t, w.Body.String(), jsonStr(ErrSignatureMismatch))
+}
