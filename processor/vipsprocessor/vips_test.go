40a20499b939e5efedd7a3b15eb4438f7729ee28
Adrian Shum <adrian@cshum.com>
2022-06-08 12:23:00 +0800

    test(vips): vips processor no-ops tests

--- processor/vipsprocessor/vips_test.go
+++ processor/vipsprocessor/vips_test.go
@@ -51,7 +51,7 @@ func TestVipsProcessor(t *testing.T) {
 			{name: "export avif", path: "filters:format(avif):quality(70)/gopher-front.png", checkTypeOnly: true},
 			{name: "export tiff", path: "filters:format(tiff):quality(70)/gopher-front.png", checkTypeOnly: true},
 			{name: "no-ops", path: "filters:background_color():frames():frames(0):round_corner():padding():rotate():proportion():proportion(-10)/gopher-front.png"},
-			{name: "no-ops 2", path: "filters:watermark():blur(2):sharpen(2):brightness():contrast():hue():saturation():rgb():modulate()/dancing-banana.gif"},
+			{name: "no-ops 2", path: "trim/filters:watermark():blur(2):sharpen(2):brightness():contrast():hue():saturation():rgb():modulate()/dancing-banana.gif"},
 			{name: "resize center", path: "100x100/filters:quality(70):format(jpeg)/gopher.png"},
 			{name: "resize smart", path: "100x100/smart/filters:autojpg()/gopher.png"},
 			{name: "resize smart focal", path: "300x100/smart/filters:fill(white):format(jpeg):focal(589x401:1000x814)/gopher.png"},
