db0eebb1cc7f44fba5166ea0019a4e9c019fba86
Adrian Shum <adrian@cshum.com>
2022-06-15 18:34:57 +0800

    feat(imagor): fanout reader

--- blob.go
+++ blob.go
@@ -119,7 +119,7 @@ func (b *Blob) peekOnce() {
 		}
 		b.size = size
 		if reader != nil && size > 0 && size < maxBodySize && err == nil {
-			newReader := FanoutReader(reader, int(size))
+			newReader := fanoutReader(reader, int(size))
 			if r, err2 := newReader(); err2 == nil {
 				b.newReader = func() (io.ReadCloser, int64, error) {
 					fReader, err3 := newReader()
@@ -248,3 +248,67 @@ func (b *Blob) Err() error {
 func isEmpty(f *Blob) bool {
 	return f == nil || f.IsEmpty()
 }
+
+func fanoutReader(reader io.ReadCloser, size int) func() (io.ReadCloser, error) {
+	var lock sync.RWMutex
+	var consumers []chan []byte
+	var err error
+	var buf []byte
+	var cnt int
+	go func() {
+		defer func() {
+			_ = reader.Close()
+		}()
+		for {
+			b := make([]byte, 512)
+			n, e := reader.Read(b)
+			lock.Lock()
+			buf = append(buf, b[:n]...)
+			cnt += n
+			if cnt <= size {
+				for _, ch := range consumers {
+					ch <- b[:n]
+				}
+				if e != nil && e != io.EOF {
+					err = e
+				}
+			}
+			lock.Unlock()
+			if e != nil {
+				return
+			}
+		}
+	}()
+	return func() (io.ReadCloser, error) {
+		lock.Lock()
+		ch := make(chan []byte, size)
+		consumers = append(consumers, ch)
+		var cnt = len(buf)
+		var b []byte
+		r := io.NopCloser(io.MultiReader(bytes.NewReader(buf), readFunc(func(p []byte) (n int, e error) {
+			if cnt >= size {
+				return 0, io.EOF
+			}
+			lock.RLock()
+			e = err
+			lock.RUnlock()
+			if e != nil {
+				return
+			}
+			if len(b) == 0 {
+				b = <-ch
+			}
+			n = copy(p, b)
+			b = b[n:]
+			cnt += n
+			return
+		})))
+		e := err
+		lock.Unlock()
+		return r, e
+	}
+}
+
+type readFunc func(p []byte) (n int, err error)
+
+func (rf readFunc) Read(p []byte) (n int, err error) { return rf(p) }
