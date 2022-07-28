edcf503560d391969020b40f4d4c383ee59788c4
Adrian Shum <adrian@cshum.com>
2022-07-28 21:41:06 +0800

    ApplySetters

--- config/setter_test.go
+++ config/setter_test.go
@@ -12,15 +12,6 @@ func TestApplySetters(t *testing.T) {
 	fs := flag.NewFlagSet("imagaor", flag.ExitOnError)
 	nopLogger := zap.NewNop()
 	var seq []int
-	op1 := func(app *imagor.Imagor) {
-		seq = append(seq, 8)
-	}
-	op2 := func(app *imagor.Imagor) {
-		seq = append(seq, 9)
-	}
-	op3 := func(app *imagor.Imagor) {
-		seq = append(seq, 10)
-	}
 	imagor.New(ApplySetters(fs, func() (logger *zap.Logger, isDebug bool) {
 		seq = append(seq, 4)
 		return nopLogger, true
@@ -30,21 +21,27 @@ func TestApplySetters(t *testing.T) {
 		assert.Equal(t, nopLogger, logger)
 		assert.True(t, isDebug)
 		seq = append(seq, 5)
-		return op1
+		return func(app *imagor.Imagor) {
+			seq = append(seq, 8)
+		}
 	}, func(fs *flag.FlagSet, cb Callback) imagor.Option {
 		seq = append(seq, 2)
 		logger, isDebug := cb()
 		assert.Equal(t, nopLogger, logger)
 		assert.True(t, isDebug)
 		seq = append(seq, 6)
-		return op2
+		return func(app *imagor.Imagor) {
+			seq = append(seq, 9)
+		}
 	}, func(fs *flag.FlagSet, cb Callback) imagor.Option {
 		seq = append(seq, 1)
 		logger, isDebug := cb()
 		assert.Equal(t, nopLogger, logger)
 		assert.True(t, isDebug)
 		seq = append(seq, 7)
-		return op3
+		return func(app *imagor.Imagor) {
+			seq = append(seq, 10)
+		}
 	})...)
 	assert.Equal(t, []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, seq)
 }
