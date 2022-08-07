b0c37ea715067d4e20b624013a10f0b66999a63d
Adrian Shum <adrian@cshum.com>
2022-08-07 09:24:05 +0800

    refactor(vips): revamp vips dependencies

--- processor/vipsprocessor/govips.go
+++ processor/vipsprocessor/govips.go
@@ -3,6 +3,7 @@ package vipsprocessor
 // #cgo pkg-config: vips
 // #include <vips/vips.h>
 // #include <stdlib.h>
+// #include <glib.h>
 // #include "govips.h"
 import "C"
 import (
@@ -170,6 +171,45 @@ func ReadVipsMemStats(stats *MemoryStats) {
 	stats.MemHigh = int64(C.vips_tracked_get_mem_highwater())
 	stats.Allocs = int64(C.vips_tracked_get_allocs())
 	stats.Files = int64(C.vips_tracked_get_files())
+}
+
+type LogLevel int
+
+const (
+	LogLevelError    LogLevel = C.G_LOG_LEVEL_ERROR
+	LogLevelCritical LogLevel = C.G_LOG_LEVEL_CRITICAL
+	LogLevelWarning  LogLevel = C.G_LOG_LEVEL_WARNING
+	LogLevelMessage  LogLevel = C.G_LOG_LEVEL_MESSAGE
+	LogLevelInfo     LogLevel = C.G_LOG_LEVEL_INFO
+	LogLevelDebug    LogLevel = C.G_LOG_LEVEL_DEBUG
+)
+
+var (
+	currentLoggingHandlerFunction = noopLoggingHandler
+	currentLoggingVerbosity       LogLevel
+)
+
+//export govipsLoggingHandler
+func govipsLoggingHandler(messageDomain *C.char, messageLevel C.int, message *C.char) {
+	govipsLog(C.GoString(messageDomain), LogLevel(messageLevel), C.GoString(message))
+}
+
+type LoggingHandlerFunction func(messageDomain string, messageLevel LogLevel, message string)
+
+func loggingSettings(handler LoggingHandlerFunction, verbosity LogLevel) {
+	if handler != nil {
+		currentLoggingHandlerFunction = handler
+	}
+	currentLoggingVerbosity = verbosity
+}
+
+func noopLoggingHandler(_ string, _ LogLevel, _ string) {
+}
+
+func govipsLog(domain string, level LogLevel, message string) {
+	if level <= currentLoggingVerbosity {
+		currentLoggingHandlerFunction(domain, level, message)
+	}
 }
 
 func handleImageError(out *C.VipsImage) error {
