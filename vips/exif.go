5d7b2d7745acb364dfc6ed61236cd03673372f18
Adrian Shum <adrian@cshum.com>
2022-08-09 18:18:09 +0800

    feat(vips): EXIF metadata support

--- vips/exif.go
+++ vips/exif.go
@@ -7,82 +7,63 @@ import (
 	"strings"
 )
 
-var exifTags = []string{
-	"exif-ifd0-Make",
-	"exif-ifd0-Model",
-	"exif-ifd0-Orientation",
-	"exif-ifd0-XResolution",
-	"exif-ifd0-YResolution",
-	"exif-ifd0-ResolutionUnit",
-	"exif-ifd0-Software",
-	"exif-ifd0-DateTime",
-	"exif-ifd0-YCbCrPositioning",
-	"exif-ifd1-Compression",
-	"exif-ifd2-ExposureTime",
-	"exif-ifd2-FNumber",
-	"exif-ifd2-ExposureProgram",
-	"exif-ifd2-ISOSpeedRatings",
-	"exif-ifd2-ExifVersion",
-	"exif-ifd2-DateTimeOriginal",
-	"exif-ifd2-DateTimeDigitized",
-	"exif-ifd2-ComponentsConfiguration",
-	"exif-ifd2-ShutterSpeedValue",
-	"exif-ifd2-ApertureValue",
-	"exif-ifd2-BrightnessValue",
-	"exif-ifd2-ExposureBiasValue",
-	"exif-ifd2-MeteringMode",
-	"exif-ifd2-Flash",
-	"exif-ifd2-FocalLength",
-	"exif-ifd2-SubjectArea",
-	"exif-ifd2-MakerNote",
-	"exif-ifd2-SubSecTimeOriginal",
-	"exif-ifd2-SubSecTimeDigitized",
-	"exif-ifd2-ColorSpace",
-	"exif-ifd2-PixelXDimension",
-	"exif-ifd2-PixelYDimension",
-	"exif-ifd2-SensingMethod",
-	"exif-ifd2-SceneType",
-	"exif-ifd2-ExposureMode",
-	"exif-ifd2-WhiteBalance",
-	"exif-ifd2-FocalLengthIn35mmFilm",
-	"exif-ifd2-SceneCaptureType",
-	"exif-ifd3-GPSLatitudeRef",
-	"exif-ifd3-GPSLatitude",
-	"exif-ifd3-GPSLongitudeRef",
-	"exif-ifd3-GPSLongitude",
-	"exif-ifd3-GPSAltitudeRef",
-	"exif-ifd3-GPSAltitude",
-	"exif-ifd3-GPSSpeedRef",
-	"exif-ifd3-GPSSpeed",
-	"exif-ifd3-GPSImgDirectionRef",
-	"exif-ifd3-GPSImgDirection",
-	"exif-ifd3-GPSDestBearingRef",
-	"exif-ifd3-GPSDestBearing",
-	"exif-ifd3-GPSDateStamp",
-}
-
-var exifInt = map[string]bool{
-	"Orientation":           true,
-	"ResolutionUnit":        true,
-	"YCbCrPositioning":      true,
-	"Compression":           true,
-	"ExposureProgram":       true,
-	"ISOSpeedRatings":       true,
-	"MeteringMode":          true,
-	"Flash":                 true,
-	"ColorSpace":            true,
-	"PixelXDimension":       true,
-	"PixelYDimension":       true,
-	"SensingMethod":         true,
-	"ExposureMode":          true,
-	"WhiteBalance":          true,
-	"FocalLengthIn35mmFilm": true,
-	"SceneCaptureType":      true,
+var exifTags = map[string]bool{
+	"exif-ifd0-Make":                    false,
+	"exif-ifd0-Model":                   false,
+	"exif-ifd0-Orientation":             true,
+	"exif-ifd0-XResolution":             false,
+	"exif-ifd0-YResolution":             false,
+	"exif-ifd0-ResolutionUnit":          true,
+	"exif-ifd0-Software":                false,
+	"exif-ifd0-DateTime":                false,
+	"exif-ifd0-YCbCrPositioning":        true,
+	"exif-ifd1-Compression":             true,
+	"exif-ifd2-ExposureTime":            false,
+	"exif-ifd2-FNumber":                 false,
+	"exif-ifd2-ExposureProgram":         true,
+	"exif-ifd2-ISOSpeedRatings":         true,
+	"exif-ifd2-ExifVersion":             false,
+	"exif-ifd2-DateTimeOriginal":        false,
+	"exif-ifd2-DateTimeDigitized":       false,
+	"exif-ifd2-ComponentsConfiguration": false,
+	"exif-ifd2-ShutterSpeedValue":       false,
+	"exif-ifd2-ApertureValue":           false,
+	"exif-ifd2-BrightnessValue":         false,
+	"exif-ifd2-ExposureBiasValue":       false,
+	"exif-ifd2-MeteringMode":            true,
+	"exif-ifd2-Flash":                   true,
+	"exif-ifd2-FocalLength":             false,
+	"exif-ifd2-SubjectArea":             false,
+	"exif-ifd2-MakerNote":               false,
+	"exif-ifd2-SubSecTimeOriginal":      false,
+	"exif-ifd2-SubSecTimeDigitized":     false,
+	"exif-ifd2-ColorSpace":              true,
+	"exif-ifd2-PixelXDimension":         true,
+	"exif-ifd2-PixelYDimension":         true,
+	"exif-ifd2-SensingMethod":           true,
+	"exif-ifd2-SceneType":               false,
+	"exif-ifd2-ExposureMode":            true,
+	"exif-ifd2-WhiteBalance":            true,
+	"exif-ifd2-FocalLengthIn35mmFilm":   true,
+	"exif-ifd2-SceneCaptureType":        true,
+	"exif-ifd3-GPSLatitudeRef":          false,
+	"exif-ifd3-GPSLatitude":             false,
+	"exif-ifd3-GPSLongitudeRef":         false,
+	"exif-ifd3-GPSLongitude":            false,
+	"exif-ifd3-GPSAltitudeRef":          false,
+	"exif-ifd3-GPSAltitude":             false,
+	"exif-ifd3-GPSSpeedRef":             false,
+	"exif-ifd3-GPSSpeed":                false,
+	"exif-ifd3-GPSImgDirectionRef":      false,
+	"exif-ifd3-GPSImgDirection":         false,
+	"exif-ifd3-GPSDestBearingRef":       false,
+	"exif-ifd3-GPSDestBearing":          false,
+	"exif-ifd3-GPSDateStamp":            false,
 }
 
 func exifStringShort(s string) string {
 	i := strings.Index(s, " (")
-	if i > 0 {
+	if i > -1 {
 		return s[:i]
 	}
 	return s
@@ -90,13 +71,13 @@ func exifStringShort(s string) string {
 
 func vipsImageGetEXIF(img *C.VipsImage) map[string]any {
 	var exif = map[string]any{}
-	for _, tag := range exifTags {
+	for tag, atoi := range exifTags {
 		name := tag[10:]
 		value := strings.TrimSpace(exifStringShort(vipsGetMetaString(img, tag)))
 		if value == "" {
 			continue
 		}
-		if exifInt[name] {
+		if atoi {
 			exif[name], _ = strconv.Atoi(value)
 		} else {
 			exif[name] = value
