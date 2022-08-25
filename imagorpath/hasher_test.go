b0c0eabb93f2d991abc6d9afffeb4db3e371e1e4
Adrian Shum <adrian@cshum.com>
2022-08-25 20:12:34 +0800

    print path

--- imagorpath/hasher_test.go
+++ imagorpath/hasher_test.go
@@ -1,6 +1,7 @@
 package imagorpath
 
 import (
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -10,22 +11,26 @@ func TestHasher(t *testing.T) {
 	p := Params{
 		FitIn: true, Width: 16, Height: 17, Image: "foobar",
 	}
+	fmt.Println(GeneratePath(p))
 	assert.Equal(t, "d5/c2804e5d81c475bee50f731db17ee613f43262", DigestResultStorageHasher.HashResult(p))
 	p.Path = GeneratePath(p)
 	assert.Equal(t, "d5/c2804e5d81c475bee50f731db17ee613f43262", DigestResultStorageHasher.HashResult(p))
 	p = Params{
 		FitIn: true, Width: 16, Height: 17, Image: "foobar",
 	}
+	fmt.Println(GeneratePath(p))
 	assert.Equal(t, "foobar.d5c2804e5d81c475bee5", SuffixResultStorageHasher.HashResult(p))
 	p.Path = GeneratePath(p)
 	assert.Equal(t, "foobar.d5c2804e5d81c475bee5", SuffixResultStorageHasher.HashResult(p))
 	p = Params{
 		Smart: true, Width: 17, Height: 19, Image: "example.com/foobar",
 	}
+	fmt.Println(GeneratePath(p))
 	assert.Equal(t, "example.com/foobar.ddd349e092cda6d9c729", SuffixResultStorageHasher.HashResult(p))
 	p = Params{
 		VAlign: "top", Width: 166, Height: 169, Image: "foobar.jpg",
 	}
+	fmt.Println(GeneratePath(p))
 	assert.Equal(t, "foobar.45d8ebb31bd4ed80c26e.jpg", SuffixResultStorageHasher.HashResult(p))
 	p.Path = GeneratePath(p)
 	assert.Equal(t, "foobar.45d8ebb31bd4ed80c26e.jpg", SuffixResultStorageHasher.HashResult(p))
