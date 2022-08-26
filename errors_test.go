238aa9e998e8aee553e4e7c76ed13388dd5a336a
Adrian Shum <adrian@cshum.com>
2022-08-27 00:33:01 +0800

    tests

--- errors_test.go
+++ errors_test.go
@@ -3,6 +3,7 @@ package imagor
 import (
 	"context"
 	"errors"
+	"github.com/cshum/imagor/imagorpath"
 	"github.com/stretchr/testify/assert"
 	"net"
 	"net/http"
@@ -38,5 +39,8 @@ func TestWrapError(t *testing.T) {
 
 	err = &net.DNSError{IsTimeout: true}
 	assert.Equal(t, ErrTimeout, WrapError(err))
+
+	err = ErrForward{imagorpath.Params{Width: 167, Height: 169, Image: "foo"}}
+	assert.Equal(t, "167x169/foo", err.Error())
 
 }
