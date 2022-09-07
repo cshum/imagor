c90cdce89cdf44e47c299ae9f7a6fc3a677f26e8
Adrian Shum <adrian@cshum.com>
2022-09-07 16:51:39 +0800

    cleanup

--- imagorpath/hasher_test.go
+++ imagorpath/hasher_test.go
@@ -7,31 +7,19 @@ import (
 )
 
 func TestHasher(t *testing.T) {
-	p := Params{
-		FitIn: true, Width: 16, Height: 17, Image: "foobar",
-	}
-	fmt.Println(GeneratePath(p))
+	p := Parse("fit-in/16x17/foobar")
 	assert.Equal(t, "d5/c2/804e5d81c475bee50f731db17ee613f43262", DigestResultStorageHasher.HashResult(p))
-	p.Path = GeneratePath(p)
+	p.Path = ""
 	assert.Equal(t, "d5/c2/804e5d81c475bee50f731db17ee613f43262", DigestResultStorageHasher.HashResult(p))
-	p = Params{
-		FitIn: true, Width: 16, Height: 17, Image: "foobar",
-	}
-	fmt.Println(GeneratePath(p))
+	p = Parse("fit-in/16x17/foobar")
 	assert.Equal(t, "foobar.d5c2804e5d81c475bee5", SuffixResultStorageHasher.HashResult(p))
-	p.Path = GeneratePath(p)
+	p.Path = ""
 	assert.Equal(t, "foobar.d5c2804e5d81c475bee5", SuffixResultStorageHasher.HashResult(p))
-	p = Params{
-		Smart: true, Width: 17, Height: 19, Image: "example.com/foobar",
-	}
-	fmt.Println(GeneratePath(p))
+	p = Parse("17x19/smart/example.com/foobar")
 	assert.Equal(t, "example.com/foobar.ddd349e092cda6d9c729", SuffixResultStorageHasher.HashResult(p))
-	p = Params{
-		VAlign: "top", Width: 166, Height: 169, Image: "foobar.jpg",
-	}
-	fmt.Println(GeneratePath(p))
+	p = Parse("166x169/top/foobar.jpg")
 	assert.Equal(t, "foobar.45d8ebb31bd4ed80c26e.jpg", SuffixResultStorageHasher.HashResult(p))
-	p.Path = GeneratePath(p)
+	p.Path = ""
 	assert.Equal(t, "foobar.45d8ebb31bd4ed80c26e.jpg", SuffixResultStorageHasher.HashResult(p))
 }
 
