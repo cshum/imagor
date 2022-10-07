647ffe4736f6d5229af7a5d3468f87ff1bd2d1b0
Adrian Shum <adrian@cshum.com>
2022-10-07 08:24:27 +0800

    fix(imagor): fanout reader buffer

--- fanout.go
+++ fanout.go
@@ -111,12 +111,12 @@ func fanoutReader(source io.ReadCloser, size int) func() (io.Reader, io.Seeker, 
 				// proxy to full buf if ready
 				return fullBufReader.Read(p)
 			}
-			lock.RLock()
-			e = err
-			sizeCopy := size
-			closedCopy := closed[i]
-			lock.RUnlock()
 			for {
+				lock.RLock()
+				e = err
+				sizeCopy := size
+				closedCopy := closed[i]
+				lock.RUnlock()
 				if cnt >= sizeCopy {
 					return 0, io.EOF
 				}
