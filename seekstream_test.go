654877366922c0c3b1c350a9c3c4e96d2a274bc5
Adrian Shum <adrian@cshum.com>
2022-10-10 20:32:53 +0800

    feat(imagor): blob.NewReadSeeker for unknown size or over 100mb

--- seekstream_test.go
+++ seekstream_test.go
@@ -15,12 +15,6 @@ func TestSeekStream(t *testing.T) {
 	rs, err := NewSeekStream(source)
 	require.NoError(t, err)
 
-	b := make([]byte, 5)
-	n, err := rs.Read(b)
-	assert.NoError(t, err)
-	assert.Equal(t, 5, n)
-	assert.Equal(t, []byte("01234"), b)
-
 	tests := []struct {
 		off     int64
 		seek    int
@@ -30,31 +24,30 @@ func TestSeekStream(t *testing.T) {
 		readerr error
 		seekerr string
 	}{
+		{seek: -1, n: 5, want: "01234"},
 		{seek: io.SeekStart, off: 0, n: 20, want: "0123456789"},
 		{seek: io.SeekStart, off: 1, n: 1, want: "1"},
 		{seek: io.SeekCurrent, off: 1, wantpos: 3, n: 2, want: "34"},
 		{seek: io.SeekStart, off: -1, seekerr: "invalid argument"},
-		//{seek: io.SeekStart, off: 1 << 33, wantpos: 1 << 33, readerr: io.EOF},
-		//{seek: io.SeekCurrent, off: 1, wantpos: 1<<33 + 1, readerr: io.EOF},
-		{seek: io.SeekStart, off: 1 << 33, wantpos: 1 << 33},
-		{seek: io.SeekCurrent, off: 1, wantpos: 1<<33 + 1},
 		{seek: io.SeekStart, n: 5, want: "01234"},
 		{seek: io.SeekCurrent, n: 5, want: "56789"},
 		{seek: io.SeekEnd, off: -1, n: 1, wantpos: 9, want: "9"},
 	}
 
 	for i, tt := range tests {
-		pos, err := rs.Seek(tt.off, tt.seek)
-		if err == nil && tt.seekerr != "" {
-			t.Errorf("%d. want seek error %q", i, tt.seekerr)
-			continue
-		}
-		if err != nil && !strings.Contains(err.Error(), tt.seekerr) {
-			t.Errorf("%d. seek error = %q; want contains %q", i, err.Error(), tt.seekerr)
-			continue
-		}
-		if tt.wantpos != 0 && tt.wantpos != pos {
-			t.Errorf("%d. pos = %d, want %d", i, pos, tt.wantpos)
+		if tt.seek >= 0 {
+			pos, err := rs.Seek(tt.off, tt.seek)
+			if err == nil && tt.seekerr != "" {
+				t.Errorf("%d. want seek error %q", i, tt.seekerr)
+				continue
+			}
+			if err != nil && !strings.Contains(err.Error(), tt.seekerr) {
+				t.Errorf("%d. seek error = %q; want contains %q", i, err.Error(), tt.seekerr)
+				continue
+			}
+			if tt.wantpos != 0 && tt.wantpos != pos {
+				t.Errorf("%d. pos = %d, want %d", i, pos, tt.wantpos)
+			}
 		}
 		buf := make([]byte, tt.n)
 		n, err := rs.Read(buf)
@@ -77,8 +70,8 @@ func TestSeekStream(t *testing.T) {
 	assert.Equal(t, io.ErrClosedPipe, err)
 	assert.Empty(t, n64)
 
-	b = make([]byte, 1)
-	n, err = rs.Read(b)
+	b := make([]byte, 1)
+	n, err := rs.Read(b)
 	assert.Equal(t, io.ErrClosedPipe, err)
 	assert.Empty(t, n)
 	assert.Empty(t, b[0])
