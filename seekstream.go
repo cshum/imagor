ad2c06dacaf519783f8c2673c64366dfad2e640c
Adrian Shum <adrian@cshum.com>
2022-10-10 13:18:03 +0800

    seek stream progress

--- seekstream.go
+++ seekstream.go
@@ -1,17 +1,17 @@
 package imagor
 
 import (
-	"errors"
 	"io"
 	"os"
+	"sync"
 )
 
 type SeekStream struct {
-	source    io.ReadCloser
-	file      *os.File
-	reader    io.Reader
-	size      int64
-	seekReady chan struct{}
+	source io.ReadCloser
+	file   *os.File
+	size   int64
+	seeked bool
+	l      sync.RWMutex
 }
 
 func NewSeekStream(source io.ReadCloser) (*SeekStream, error) {
@@ -19,38 +19,77 @@ func NewSeekStream(source io.ReadCloser) (*SeekStream, error) {
 	if err != nil {
 		return nil, err
 	}
-	reader := io.TeeReader(source, file)
 	return &SeekStream{
-		file:   file,
-		reader: reader,
 		source: source,
+		file:   file,
 	}, nil
 }
 
 func (s *SeekStream) Read(p []byte) (n int, err error) {
-	if s.reader != nil {
-		return s.reader.Read(p)
+	s.l.RLock()
+	defer s.l.RUnlock()
+	if !s.seeked {
+		n, err = s.source.Read(p)
+		if n > 0 {
+			s.size += int64(n)
+			if n, err := s.file.Write(p[:n]); err != nil {
+				return n, err
+			}
+		}
+		return
 	}
-	<-s.seekReady
 	if s.file != nil {
 		return s.file.Read(p)
 	}
-	return 0, errors.New("todo")
+	return 0, io.ErrClosedPipe
 }
 
 func (s *SeekStream) Seek(offset int64, whence int) (int64, error) {
-	s.reader = nil
-	return 0, errors.New("todo")
+	s.l.Lock()
+	defer s.l.Unlock()
+	if !s.seeked {
+		s.seeked = true
+		if s.file != nil {
+			filename := s.file.Name()
+			_ = s.file.Close()
+			var err error
+			if s.file, err = os.Open(filename); err != nil {
+				_ = s.Close()
+				return 0, err
+			}
+		} else {
+			return 0, io.ErrClosedPipe
+		}
+		n, err := io.Copy(s.file, s.source)
+		s.size += n
+		if err != nil {
+			return 0, err
+		}
+	}
+	if s.file != nil {
+		return s.file.Seek(offset, whence)
+	}
+	return 0, io.ErrClosedPipe
 }
 
-func (s *SeekStream) Close() error {
+func (s *SeekStream) Close() (err error) {
+	s.l.Lock()
+	defer s.l.Unlock()
 	if s.file != nil {
+		filename := s.file.Name()
 		_ = s.file.Close()
+		_ = os.Remove(filename)
+		s.file = nil
 	}
-	return s.source.Close()
+	if s.source != nil {
+		err = s.source.Close()
+		s.source = nil
+	}
+	return
 }
 
 func (s *SeekStream) Size() int64 {
-	<-s.seekReady
+	s.l.RLock()
+	defer s.l.RUnlock()
 	return s.size
 }
