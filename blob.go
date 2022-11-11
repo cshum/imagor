750287186dab7127dc21ff83473e26c4d1265f75
Adrian Shum <adrian@cshum.com>
2022-11-12 00:34:54 +0800

    refactor(imagor): hybrid read seeker

--- blob.go
+++ blob.go
@@ -174,10 +174,10 @@ func (h *hybridReadSeeker) Seek(offset int64, whence int) (n int64, err error) {
 	if h.seeker != nil {
 		return h.seeker.Seek(offset, whence)
 	}
-	_ = h.reader.Close()
 	if h.seeker, _, err = h.newReadSeeker(); err != nil {
 		return
 	}
+	_ = h.reader.Close()
 	h.reader = h.seeker
 	return h.seeker.Seek(offset, whence)
 }
