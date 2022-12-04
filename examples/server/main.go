bf4fcf04a5963ff964f330cb8cae7416f6cef854
Adrian Shum <adrian@cshum.com>
2022-12-05 00:00:27 +0800

    chore: add server example

---
+++ examples/server/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/cshum/imagor"
+	"github.com/cshum/imagor/imagorpath"
+	"github.com/cshum/imagor/loader/httploader"
+	"github.com/cshum/imagor/server"
+	"github.com/cshum/imagor/storage/filestorage"
+	"github.com/cshum/imagor/vips"
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger := zap.Must(zap.NewProduction())
+
+	server.New(
+		imagor.New(
+			imagor.WithLogger(logger),
+			imagor.WithUnsafe(true),
+			imagor.WithProcessors(vips.NewProcessor()),
+			imagor.WithLoaders(httploader.New()),
+			imagor.WithStorages(filestorage.New("./")),
+			imagor.WithResultStorages(filestorage.New("./")),
+			imagor.WithResultStoragePathStyle(imagorpath.SuffixResultStorageHasher),
+		),
+		server.WithPort(8000),
+		server.WithLogger(logger),
+	).Run()
+}
