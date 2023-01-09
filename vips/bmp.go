bf527e78772807068f5068a5f53c1f70c1611b94
Adrian Shum <adrian@cshum.com>
2023-01-09 15:33:32 +0800

    feat(vips): bmp support (#294)
    * feat(vips): BMP support
    
    * test: update golden files
    
    * feat(vips): BMP fallback support

---
+++ vips/bmp.go
@@ -0,0 +1,23 @@
+package vips
+
+import (
+	"golang.org/x/image/bmp"
+	"image"
+	"image/draw"
+	"io"
+)
+
+func loadImageFromBMP(r io.Reader) (*Image, error) {
+	img, err := bmp.Decode(r)
+	if err != nil {
+		return nil, err
+	}
+	rect := img.Bounds()
+	size := rect.Size()
+	rgba, ok := img.(*image.RGBA)
+	if !ok {
+		rgba = image.NewRGBA(rect)
+		draw.Draw(rgba, rect, img, rect.Min, draw.Src)
+	}
+	return LoadImageFromMemory(rgba.Pix, size.X, size.Y, 4)
+}
