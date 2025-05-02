73b68e83bf827afcd266190f8bae78f45dea687d
Adrian Shum <adrian@cshum.com>
2025-05-02 20:12:45 +0800

    fix embed_multi_page_image

--- vips/vips.c
+++ vips/vips.c
@@ -126,13 +126,14 @@ int embed_multi_page_image(VipsImage *in, VipsImage **out, int left, int top, in
   int n_pages = in->Ysize / page_height;
 
   VipsImage **page = (VipsImage **) vips_object_local_array(base, n_pages);
+  VipsImage **embedded_page = (VipsImage **) vips_object_local_array(base, n_pages);
   VipsImage **copy = (VipsImage **) vips_object_local_array(base, 1);
 
   // split image into cropped frames
   for (int i = 0; i < n_pages; i++) {
     if (
       vips_extract_area(in, &page[i], 0, page_height * i, in_width, page_height, NULL) ||
-      vips_embed(page[i], &page[i], left, top, width, height, "extend", extend, NULL)
+      vips_embed(page[i], &embedded_page[i], left, top, width, height, "extend", extend, NULL)
     ) {
       g_object_unref(base);
       return -1;
@@ -141,7 +142,7 @@ int embed_multi_page_image(VipsImage *in, VipsImage **out, int left, int top, in
   // reassemble frames and set page height
   // copy before modifying metadata
   if(
-    vips_arrayjoin(page, &copy[0], n_pages, "across", 1, NULL) ||
+    vips_arrayjoin(embedded_page, &copy[0], n_pages, "across", 1, NULL) ||
     vips_copy(copy[0], out, NULL)
   ) {
     g_object_unref(base);
